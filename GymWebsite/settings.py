1ba662a9e8192a7b8f714e6bd31b6baaae1a8ced
Natanael Jop <natanael.jop@icloud.com>
2024-05-18 13:22:40 +0200

    Update

--- GymWebsite/settings.py
+++ GymWebsite/settings.py
@@ -89,12 +89,18 @@ WSGI_APPLICATION = 'GymWebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import os
+import dj_database_url
+
+if not DEBUG:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
